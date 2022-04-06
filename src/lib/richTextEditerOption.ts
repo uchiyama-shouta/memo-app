f1f5268c88f56190c207529371a21a6442edea88
utiyama <ninjin0604@gmail.com>
2022-04-07 00:10:35 +0900

    リファクタリング

---
+++ src/lib/richTextEditerOption.ts
@@ -0,0 +1,9 @@
+import { ToolbarControl } from "@mantine/rte/lib/components/Toolbar/controls";
+
+export const option: ToolbarControl[][] = [
+  ["bold", "strike", "italic", "underline", "clean"],
+  ["h1", "h2", "h3", "h4"],
+  ["unorderedList", "orderedList"],
+  ["blockquote", "code", "codeBlock"],
+  ["alignLeft", "alignCenter", "alignRight"],
+];
